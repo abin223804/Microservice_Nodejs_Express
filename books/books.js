4e1d10a899de50e0435866e48cb2870f9b7bc5d7
Abin_haashIndia <haashtechnologies@gmail.com>
2024-11-20 12:25:20 +0530

    first section(books) basic completed

--- books/books.js
+++ books/books.js
@@ -1,7 +1,11 @@
 import express from 'express';
 const app = express();
 
+
+app.get('/', (req, res) => {
+res.send("This is the books service")
+})
 app.listen(3000,()=>{
     console.log("Server is running  ! -- This is our Book service!");
     
-})
+})  
