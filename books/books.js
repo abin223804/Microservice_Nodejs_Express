3b95f62214dc28cc9f0fb054b8968fc0b4ae0a0a
Abin_haashIndia <haashtechnologies@gmail.com>
2024-11-20 16:25:43 +0530

    db connected for bookservice

--- books/books.js
+++ books/books.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 
 
+mongoose.connect("mongodb+srv://abinschandran:XPeTs1spqwqxQ2iG@cluster0.xpc7j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+    console.log("Connected to MongoDB!");
+});
 app.get('/', (req, res) => {
 res.send("This is the books service")
 })
