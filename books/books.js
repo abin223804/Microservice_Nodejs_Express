ae185faf7cbc0a756c645f24279a2c5e0241fd6e
Abin_haashIndia <haashtechnologies@gmail.com>
2024-11-21 11:15:24 +0530

    get data set

--- books/books.js
+++ books/books.js
@@ -1,43 +1,65 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 
-import Book from './Book.js';
+import Book from "./Book.js";
 const app = express();
-
 
 app.use(express.json());
 
-
-mongoose.connect("mongodb+srv://abinschandran:XPeTs1spqwqxQ2iG@cluster0.xpc7j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+mongoose
+  .connect(
+    "mongodb+srv://abinschandran:XPeTs1spqwqxQ2iG@cluster0.xpc7j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
     console.log("Connected to MongoDB!");
+  });
+app.get("/", (req, res) => {
+  res.send("This is the books service");
 });
-app.get('/', (req, res) => {
-res.send("This is the books service")
-})
 
-app.post('/books', (req, res) => {
-
-    const newBook ={
-        title: req.body.title,
-        author: req.body.author,
-        numberPages: req.body.numberPages,
-        publisher: req.body.publisher,
-    }
+app.post("/books", (req, res) => {
+  const newBook = {
+    title: req.body.title,
+    author: req.body.author,
+    numberPages: req.body.numberPages,
+    publisher: req.body.publisher,
+  };
   const book = new Book(newBook);
 
-  book.save().then(()=>{
-
-    res.json(book);
-    console.log("New book created successfully");
-    
-  }).catch((err)=>{
-    if(err){
+  book
+    .save()
+    .then(() => {
+      res.json(book);
+      console.log("New book created successfully");
+    })
+    .catch((err) => {
+      if (err) {
         throw err;
-    }
-  })
+      }
+    });
+});
 
-})
-app.listen(3000,()=>{
-    console.log("Server is running  ! -- This is our Book service!");
+app.get("/books",(req, res) => {
+
+    Book.find()
+       .then((books) => {
+        console.log(books);
+        
+            res.json(books);
+        })
+       .catch((err) => {
+            if (err) {
+                throw err;
+            }
+        });
     
-})  
+})
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("Server is running  ! -- This is our Book service!");
+});
